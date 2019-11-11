6d750310bd6f0c0a0f07085dbd176d4cf3ffa601
Jakub Kuchar <jakub.kuchar@hotmail.com>
2019-11-11 16:11:06 +0700

    Update hash.rb

--- lib/swanest/core_ext/hash.rb
+++ lib/swanest/core_ext/hash.rb
@@ -4,11 +4,11 @@ class Hash
   #
   # @param hash [Hash] The hash to merge
   # @return [Hash]
-  def deep_merge(hash)
+  def swanest_deep_merge(hash)
     target = self.dup
     hash.keys.each do |key|
       if hash[key].is_a?(Hash) && self[key].is_a?(Hash)
-        target[key] = target[key].deep_merge(hash[key])
+        target[key] = target[key].swanest_deep_merge(hash[key])
         next
       end
       target[key] = hash[key]
